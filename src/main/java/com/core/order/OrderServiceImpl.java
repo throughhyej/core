4bfdab575daf110783c4f6494a34318a2ff39f07
throughhyej <hyejeyh@gmail.com>
2022-02-06 10:59:08 +0900

    의존관계 자동 주입 (lombok 적용, 테스트 겸 OrderServiceImpl.java의 생성자 lombok으로 대체)

--- src/main/java/com/core/order/OrderServiceImpl.java
+++ src/main/java/com/core/order/OrderServiceImpl.java
@@ -2,10 +2,12 @@ package com.core.order;
 
 import com.core.discount.DiscountPolicy;
 import com.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // SOLID의 DIP 위반
@@ -16,11 +18,12 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+      /* lombok @RequiredArgsConstructor로 대체 */
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
